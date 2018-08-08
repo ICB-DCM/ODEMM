79c1e29f3f616c6f3f1145d1441f867c8dbeb88c
Carolin Loos <carolin.loos@helmholtz-muenchen.de>
2018-08-08 17:36:46 +0200

    cleanup
    removed n=2 as special case and directly treat general n

--- distributions/norm/func_Sigma_norm.m
+++ distributions/norm/func_Sigma_norm.m
@@ -1,5 +1,5 @@
 function Sigma = func_Sigma_norm(t,x,xi,n_dim,varargin)
-% This function maps the means and variances to \f$\boldsymbol{\Sigma}\f$ 
+% This function maps the means and variances to \f$\boldsymbol{\Sigma}\f$
 % of the multivariate normal distribution.
 %
 % USAGE:
@@ -10,45 +10,37 @@ function Sigma = func_Sigma_norm(t,x,xi,n_dim,varargin)
 % x: vector including means and variances
 % xi: (not used, included for consistency and possible extensions)
 % n_dim: dimension of measurement
-% varargin: 
+% varargin:
 %   * noise: parameter for measurement noise
 %   * noisemodel: (so far only ''additive'' supported)
 %
 % Return values:
-% Sigma: (n_t x n_dim x n_dim) \f$\Sigma\f$ of the multivariate normal distribution. 
+% Sigma: (n_t x n_dim x n_dim) \f$\Sigma\f$ of the multivariate normal distribution.
 
-noise = [0;0];
+noise = zeros(n_dim,1);
 noisemodel = 'additive';
-if nargin >= 4
-   noise = varargin{1};
-   if nargin >=5
-       noisemodel = varargin{2};
-   end
+if nargin > 4
+    noise = varargin{1};
+    if nargin >5
+        noisemodel = varargin{2};
+    end
 end
 if ~strcmp(noisemodel,'additive')
-    error('Only noisemodel additive possible!')
+    error('Only noise model additive possible!')
 end
 Sigma = zeros(numel(t),n_dim,n_dim);
-if ~isequal(n_dim,2)
-    error('Todo: adapt for n_dim > 2')
-end
-if n_dim==2
-    for k=1:numel(t)
-     Sigma(k,:,:) = [x(k,3)+noise(1),x(k,4); x(k,4),x(k,5)+noise(2)];
-    end
-else
-    for k = 1:numel(t)
-        n = n_dim+1;
-        for i = 1:n_dim
-            for j = 1:n_dim
-                if isequal(i,j)
-                    Sigma(k,i,j) = x(k,n)+noise(i);
-                else                   
-                    Sigma(k,i,j) = x(k,n);
-                    Sigma(k,j,i) = x(k,n);
-                end
-                n=n+1;
+
+for k = 1:numel(t)
+    n = n_dim+1;
+    for i = 1:n_dim
+        for j = 1:n_dim
+            if isequal(i,j)
+                Sigma(k,i,j) = x(k,n)+noise(i);
+            else
+                Sigma(k,i,j) = x(k,n);
+                Sigma(k,j,i) = x(k,n);
             end
+            n=n+1;
         end
     end
 end
