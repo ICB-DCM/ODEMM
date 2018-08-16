059afda98730f1de49e41b1afd90440cb520d5b6
Carolin Loos <carolin.loos@helmholtz-muenchen.de>
2018-08-16 09:23:32 +0200

    skew norm random number generation

--- distributions/skew_norm/skewnormrnd.m
+++ distributions/skew_norm/skewnormrnd.m
@@ -1,11 +1,11 @@
 function Y = skewnormrnd(mu,Sigma,delta,n)
+% Random number generation for multivariate skew normal distribution
+% according to Pyne (PNAS, 2009).
 
-Omega = Sigma + delta*delta';
-D = sqrt(diag(Omega));
-invD = inv(D);
-Omega_ = invD*Omega*invD;
-U = mvnrnd(mu,Omega_,n)';
+Y = rand(size(Sigma,1),n);
+U = mvnrnd(mu,Sigma,n)';
 U0 = randn(n,1);
+
 for i = 1:n
     Y(:,i) = delta.*abs(U0(i)) + U(:,i);
 end
