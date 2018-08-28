02de2728341ae08395756afb4e7875756462fe40
Carolin Loos <carolin.loos@helmholtz-muenchen.de>
2018-08-28 15:38:02 +0200

    students t bugfixes

--- distributions/students_t/func_dmudxi_students_t.m
+++ distributions/students_t/func_dmudxi_students_t.m
@@ -1,4 +1,4 @@
-function dmudxi = func_dmudxi_student_t(t,x,dxdxi,Sigma,dSigmadxi,xi,u,n_dim)
+function dmudxi = func_dmudxi_student_t(t,x,dxdxi,xi,u,n_dim)
 % This function calculates the derivative of \f$\boldsymbol{\mu}\f$ of the
 % (multivariate) normal distribution.
 %
@@ -6,8 +6,6 @@ function dmudxi = func_dmudxi_student_t(t,x,dxdxi,Sigma,dSigmadxi,xi,u,n_dim)
 % t: time vector (not used, included for consistency and possible extensions)
 % x: vector of means and variances (not used, included for consistency and possible extensions)
 % dxdxi: derivatives of means and variances
-% Sigma: (not used, included for consistency and possible extensions)
-% dSigmadxi: (not used, included for consistency and possible extensions)
 % xi: parameter vector (not used, included for consistency and possible extensions)
 % u: input (not used, included for consistency and possible extensions)
 % n_dim: dimension of measurement
