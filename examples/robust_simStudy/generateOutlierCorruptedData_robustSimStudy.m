56de1facb2b5531fd7f634093c5a9023711cc451
Carolin Loos <carolin.loos@helmholtz-muenchen.de>
2019-07-28 20:31:53 +0200

    Create generateOutlierCorruptedData_robustSimStudy.m

---
+++ examples/robust_simStudy/generateOutlierCorruptedData_robustSimStudy.m
@@ -0,0 +1,63 @@
+% This script generates the outlier-corrupted data for the simulation study.
+
+clear all
+close all
+clc
+
+% Models included in the manuscript number 1, 3 and 4
+modelnames = {'conversionReaction',... % 1D data
+    'diffProteinExpression',... % 2D data
+    'twoStageGeneExpression',... % 1D data
+    'diffProteinExpression'}; % 1D data
+
+n_cells = [50,100,500,1000];
+tps{1}=[0,0.5,1,2,4];
+tps{2}=[0,0.5,2,4];
+tps{3}=[0,0.5,2];
+t = tps{1};
+
+% outlierPerc = 0.02;
+% outlierstr = 'outlier2_zeros';
+ 
+% outlierPerc = 0.05;
+% outlierstr = 'outlier5_dublets';
+
+outlierPerc = 0.1;
+outlierstr = 'outlier10_unif';
+
+for m = [1,3,4]
+    for it = 1:3
+        t = tps{it};
+        for ic = 1:4
+            for set = 1:3
+                load(['./data/data_model' num2str(m) '_' modelnames{m} '_' ...
+                    num2str(n_cells(ic)) ...
+                    'cells_' num2str(length(t)) 'tps_' num2str(set) 'paramsetD'],'D')
+                nOutlier = round(n_cells(ic)*outlierPerc);
+                indOutlier = randperm(n_cells(ic));
+                indOutlier = indOutlier(1:nOutlier);
+                switch outlierstr
+                    case 'outlier2_zeros'
+                        for iit = 1:length(t)
+                            D.y(:,iit,indOutlier,:) = 0;
+                        end
+                    case 'outlier5_dublets'
+                        for iit = 1:length(t)
+                            D.y(:,iit,indOutlier,:) = 2*D.y(:,iit,indOutlier,:);
+                        end
+                    case 'outlier10_unif'
+                        for iit = 1:length(t)
+                            interv = max(squeeze(D.y(:,iit,:,:)))-min(squeeze(D.y(:,iit,:,:)));
+                            lb = max(0,min(squeeze(D.y(:,iit,:,:)))-interv*0.25);
+                            ub = max(squeeze(D.y(:,iit,:,:)))+interv*0.25;
+                            D.y(:,iit,indOutlier,:) = round(rand(length(indOutlier),1)*(lb-ub)+ub);
+                        end
+                end
+                save(['./dataOutlier/data_model' num2str(m) '_' ...
+                    modelnames{m} '_' num2str(n_cells(ic)) ...
+                    'cells_' num2str(length(t)) 'tps_' num2str(set) ...
+                    'paramset_' outlierstr],'D')
+            end
+        end
+    end
+end
