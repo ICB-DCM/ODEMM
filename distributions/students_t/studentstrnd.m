2af34a6f6f345a43bd9b7615d7062b9f6737c8a8
Carolin Loos <carolin.loos@helmholtz-muenchen.de>
2019-07-29 12:33:45 +0200

    Delete studentstrnd.m

--- distributions/students_t/studentstrnd.m
+++
@@ -1,9 +0,0 @@
-function Y = studentstrnd(mu,Sigma,nu,n)
-% Random number generation for multivariate t distribution
-% according to Pyne (PNAS, 2009).
-
-lambda = gamrnd(nu/2,2/nu,n,1);
-
-for i = 1:n
-    Y(:,i) = mvnrnd(mu,Sigma./lambda(i));
-end
